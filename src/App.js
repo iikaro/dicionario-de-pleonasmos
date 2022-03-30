cf818477c5420287b82086aa201e730660b978cd
Icaro <icarostn@gmail.com>
2022-03-30 20:37:01 +0200

    feat: change main page welcome text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem-vindo ao primeiro dicionário de pleonasmos online da língua portuguesa!
         </p>
         <a
           className="App-link"
