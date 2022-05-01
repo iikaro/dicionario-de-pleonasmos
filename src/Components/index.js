484548e2e2483cb5ecbac033c45eb73b24631399
Icaro <icarostn@gmail.com>
2022-05-01 11:37:32 +0200

    feat: include new index to condense component imports

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Pages/Home";
+export { default as About } from "./Pages/About";
+export { default as Contact } from "./Pages/Contact";
+export { default as Blog } from "./Blog/Blog";
+export { default as Posts } from "./Blog/Posts";
+export { default as Post } from "./Blog/Post";
+export {default as PleonasmList} from "./Pages/PleonasmsList"
