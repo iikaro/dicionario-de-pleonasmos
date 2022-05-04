14ebd70d68e4082e40abd58a059b2d39fb27f7b1
Icaro <icarostn@gmail.com>
2022-05-04 19:13:08 +0200

    fix: remove random character

--- src/Components/Pages/Entry/Entry.js
+++ src/Components/Pages/Entry/Entry.js
@@ -24,7 +24,6 @@ export const Entry = () => {
                 {DictData[27].secondWordClass}
             </EntrySecondPartDefinition>
 
-a
             <EntryExplanation>
                 {DictData[27].explanation}
             </EntryExplanation>
