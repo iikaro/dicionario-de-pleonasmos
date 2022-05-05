bb3d7aac459e0d7d9e4e1f5075ea820c34c17cc7
Icaro <icarostn@gmail.com>
2022-05-05 19:40:01 +0200

    Deploy React app to GitHub Pages

--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-export { Home } from "./Pages/Home/Home";
-export { About } from "./Pages/About/About";
-export { Contact } from "./Pages/Contact/Contact";
-export { Entry } from "./Pages/Entry/Entry"
